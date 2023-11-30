9a6046392ea49e1ff4ba9ff844a30fbdb44f3953
Clinton Nmereole <clintonnmereole@gmail.com>
2023-11-29 21:32:33 -0600

    started parse file, made parsing function that works for symbols

--- src/parser.zig
+++ src/parser.zig
@@ -1,12 +1,83 @@
-const main1 = @import("main.zig");
-const expression = main1.Expression;
-const rule = main1.Rule;
-
 const std = @import("std");
+const Lexer = @import("lexer.zig");
+const Token = Lexer.Token;
+
+const Zoq = @import("main.zig");
+const Expression = Zoq.Expression;
+
+const ArrayList = std.ArrayList;
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
+
+var arraylist = ArrayList(u8).init(allocator);
+
+pub inline fn testparse(
+    comptime buffer: []const u8,
+) !void {
+    comptime var lexer = Lexer{};
+    //const expected_token: Token = .eof;
+    comptime var i = 0;
+    inline while (i < buffer.len) {
+        const actual_token = lexer.next(buffer);
+        std.debug.print("{}\n", .{actual_token});
+        try arraylist.append(buffer[i]);
+        i += 1;
+    }
+    std.debug.print("{s}\n", .{arraylist.items});
+}
+
+pub inline fn parse_symbol(
+    comptime buffer: []const u8,
+) !Expression {
+    comptime var lexer = Lexer{};
+    const actual_token = lexer.next(buffer);
+    const expected_type = .identifier;
+    comptime if (actual_token != expected_type) return error.NotSymbol;
+    return Zoq.sym(actual_token.identifier);
+}
+
+pub inline fn parse_function(
+    comptime buffer: []const u8,
+) !Expression {
+    comptime var lexer = Lexer{};
+    comptime var function: []const u8 = undefined;
+    comptime if (buffer.len < 3) return error.NotFunction;
+    //const expected_type = .eof;
+    comptime var current_token = lexer.next(buffer);
+    comptime var arr = [_]Expression{undefined} ** 5;
+    comptime var i = 0;
+    inline while (current_token != .eof and i < buffer.len) {
+        comptime var prev_token = current_token;
+        current_token = lexer.next(buffer);
+        std.debug.print("prev_token: {}\n", .{prev_token});
+        std.debug.print("current_token: {}\n", .{current_token});
+        if (prev_token == .identifier and current_token == .openParen) {
+            comptime var function_name = prev_token.identifier;
+            function = function_name;
+        }
+        comptime if (current_token == .comma and prev_token == .identifier) {
+            arr[i] = Zoq.sym(prev_token.identifier);
+        };
+        comptime if (current_token == .closeParen and prev_token == .identifier) {
+            arr[i] = Zoq.sym(prev_token.identifier);
+        };
+        i += 1;
+    }
+
+    return Zoq.fun(function, &arr);
+}
 
 pub fn main() !void {
-    var new_expression = expression{ .function = .{ .name = "add", .args = &.{ expression{ .symbol = .{ .str = "a" } }, expression{ .symbol = .{ .str = "b" } } } } };
-    std.debug.print("{}\n", .{@TypeOf(expression)});
-    std.debug.print("{}\n", .{@TypeOf(rule)});
-    std.debug.print("{}\n", .{new_expression});
+    try testparse("add(a,b)");
+    var a = try parse_symbol("abr");
+    std.debug.print("{}\n", .{a});
+    std.debug.print("{}\n", .{@TypeOf(a)});
+    var b = try parse_function("add(a,b)");
+    std.debug.print("{}\n", .{b});
+}
+
+test "parser" {
+    var a_symbol = Zoq.sym("a");
+    var a = try parse_symbol("a");
+    try std.testing.expect(a.eql(a_symbol));
 }
